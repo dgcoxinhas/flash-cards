912642f799a41a6f1e688de8e5a163da6512ad8c
Duda Chambó <160850761+dgcoxinhas@users.noreply.github.com>
2024-10-11 11:37:35 -0300

    x

---
+++ perguntas.js
@@ -0,0 +1,102 @@
+criarCartao (
+    'matematica',
+    'Qual é a fórmula de Pitágoras?',
+    'h²= a²+b²'
+    )
+    
+    criarCartao (
+    'biologia',
+    'O que é o fenótipo?',
+    'o fenótipo é o conjunto de traços de um indivíduo, características físicas.'
+    )
+    criarCartao (
+    'geografia',
+    'quais são os paises em que a UA media conflitos? ',
+    'Republica Democrática do Congo, Líbia, Somália e Saldão do Sul '
+    )
+    criarCartao (
+    'historia',
+    'em qual governo foi imposto a Ditadura Militar?',
+    'no governo de João Goulart'
+    )
+    criarCartao (
+    'inglês',
+    'qual é o plural de mulher em inglês?',
+    'women'
+    )
+    criarCartao (
+    'quimica',
+    'qual é a o grupo funcional do composto HC³-CH²-OH?',
+    'álcool '
+    )
+    criarCartao (
+    'portugues',
+    'quais são os tipos de sujeito?',
+    'simples, composto, oculto, determinado, indeterminado, desinencial'
+    )
+    criarCartao (
+    'sociologia',
+    'o que é a sociologia para Weber?',
+    'ciencia q estuda e interpreta a ação social para explica-la em seus cursos e efeitos'
+    )
+    criarCartao (
+    'programacao',
+    'qual é a função do código display: none?',
+    'sua função é sumir'
+    )
+    criarCartao (
+    'fisica',
+    'qual a fórmula da corrente em paralela?',
+    'If= I¹+I²+I³......'
+    )
+    criarCartao (
+    'matematica',
+    'quais são as fórmulas para os quadrantes de um arco?',
+    'segundo quadrante: 180 - alfa, terceiro quadrante: 180 + alfa, quarto quadrante: 380 - alfa'
+    )
+    criarCartao (
+    'biologia',
+    'qual é a diferença de órgãos homólogos e análogos ?',
+    'os homólogos compartilham a mesma origem embrionária e os análogos apesar de terem a mesma função tem origens diferentes'
+    )
+    criarCartao (
+    'geografia',
+    'quais são os modelos de produção?',
+    'Taylorismo, Fordismo e Toyotismo'
+    )
+    criarCartao (
+    'historia',
+    'quais são as características do Nazifascismo?',
+    'nacionalismo extremo, totalitarismo Militar, anti liberalismo, anti comunismo e partido único'
+    )
+    criarCartao (
+    'ingles',
+    'quais são os pronomes em inglês?',
+    'I, You, She, He, It, They, Her, Him, Them, Us, We, Me, Myself ,Herself, Himself e Itself. '
+    )
+    criarCartao (
+    'quimica',
+    'quais são os fatores q alteram o velocidade de uma reação?',
+    'superfície de contato, catalizador, temperatura, pressão, concentração'
+    )
+    criarCartao (
+    'portugues',
+    'quais os tipos de tese?',
+    'declaração, definição, separação, alusao histórica'
+    )
+    criarCartao (
+    'sociologia',
+    'oq são os meios de produção definidos por Marx?',
+    'os meios de produção são conjuntos, formados por meios q objetos de trabalho ou tudo aquilo q medeia o trabalho humano e a natureza na sua transformação'
+    )
+    criarCartao (
+    'programacao',
+    'oq é java script?',
+    'e a linguagem de programação q permite a criação de páginas web interativas com conteúdo q se arualiza'
+    )
+    criarCartao (
+    'fisica',
+    'oq é um gerador?',
+    'dispositivo q converte vários tipos de energia em energia elétrica'
+    )
+    
