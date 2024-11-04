23acd0a428d70e00aaf4258a0f812a022b78cd3d
Duda Chambó <160850761+dgcoxinhas@users.noreply.github.com>
2024-11-04 07:48:18 -0300

    c

--- projeto2.js
+++ projeto2.js
@@ -3,7 +3,7 @@ function criarCartao(categoria, pergunta, resposta){
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
     cartao.innerHTML = `
-    <div class="conteudo-cartao">
+    <div class="conteudo-cartao"  onclick = function mostraResposta(card) >
     <h3>${categoria}</h3>
     <div class="pergunta-cartao">
         <p>${pergunta}</p>
@@ -17,4 +17,19 @@ function criarCartao(categoria, pergunta, resposta){
 
     container.appendChild(cartao)
 }
+
+function mostraResposta(card){
+    const resposta= document.querySelector('.resposta-cartao')
+
+    if(resposta.computedStyleMap.display === 'none'){
+        resposta.computedStyleMap.display = 'block'
+       
+    }
+
+    else{
+        resposta.computedStyleMap.display = 'none'
+
+    }
+}
+
 
